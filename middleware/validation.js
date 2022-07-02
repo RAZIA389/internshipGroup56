22347e5217f7a7cf4f8af67fc1da049815b60310
RAZIA389 <106535085+RAZIA389@users.noreply.github.com>
2022-07-02 20:33:55 +0530

    Update validation.js

--- middleware/validation.js
+++ middleware/validation.js
@@ -54,7 +54,7 @@ const isBoolean = function(value){
 //Value Validation
 const isValid = function(value){
     if(typeof value ==='undefined' || value ===null)  return false
-    if(typeof value !=='string' || value.trim().length ===0)return false
+    if(typeof value ==='string' || value.trim().length ===0)return false
     return true
 }
 
